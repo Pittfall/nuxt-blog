4bc26e9e5d6b43e1bc5a98c20c5d9f001ad83d33
Andrew Rusu <liviuandrei@bungalow.com>
2020-08-06 18:31:58 -0400

    Checkpoint getting the inital meat for the blog.

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="posts-list">
+        <PostPreview 
+            id="1"
+            :is-admin="isAdmin"
+            thumbnail="https://tech-boys.com/wp-content/uploads/2019/12/womenintech.jpg"
+            title="First"
+            previewText="This is my first post!" />
+        <PostPreview 
+            id="2"
+            thumbnail="https://tech-boys.com/wp-content/uploads/2019/12/womenintech.jpg"
+            title="Second"
+            previewText="This is my second post!" />
+        <PostPreview 
+            id="3"
+            thumbnail="https://tech-boys.com/wp-content/uploads/2019/12/womenintech.jpg"
+            title="Third"
+            previewText="This is my third post!" />
+    </section>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postid/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '~/components/Admin/AdminPostForm'
+
+export default {
+    layout: 'admin',
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Andrew',
+                title: 'My aweseome post',
+                thumbnailLink: 'https://tech-boys.com/wp-content/uploads/2019/12/womenintech.jpg',
+                content: 'Super amazing, thanks for that!'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
